0de750025fdaea99f02f691eecd85e1f9031daec
clamrich369-web <clamrich369@gmail.com>
2026-02-12 16:23:20 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import *
 from datetime import datetime
 
-TOKEN = "YOUR_NEW_TOKEN"
+TOKEN = "8543932711:AAFBzavfn2MunYAvnCKWiAEisUIyEmT04XQ"
 ADMIN_IDS = [289763127]
 
 bot = Bot(token=TOKEN)
